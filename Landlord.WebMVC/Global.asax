ac6ca74b5ab1a1d2d73a526a4a38ae7a44864a19
timdeeter <timdeeter1@gmail.com>
2018-08-15 13:35:48 -0400

    Add project files.

---
+++ Landlord.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Landlord.WebMVC.MvcApplication" Language="C#" %>
---
+++ Landlord.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Landlord.WebMVC.Startup))]
+namespace Landlord.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
